584c259e766047b98ea8d26a211eaf9154ab0985
Indra Prabawa <joeprabawa@gmail.com>
2018-07-19 06:48:36 +0800

    setting apikey & testing fetch

--- app.js
+++ app.js
@@ -13,24 +13,25 @@ window.location.hash = '';
 
 // Set token
 let _token = hash.access_token;
+
 const authEndpoint = 'https://accounts.spotify.com/authorize';
 
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = '753819e3242f444aabc7cadacd11de5b';
 const redirectUri = 'http://localhost:5500';
 const scopes = [
-  'user-read-private'
+  'user-top-read'
 ];
 
 // If there is no token, redirect to Spotify authorization
 if (!_token) {
-  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token`;
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
 }
 
 const url = 'https://api.spotify.com/v1/me/playlists';
 const options = {
     headers : {
-        'Authorization' :  `Bearer ${_token}`;
+        'Authorization' :  `Bearer ${_token}`
     }
 }
 
